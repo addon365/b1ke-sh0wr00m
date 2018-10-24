a936834a8c4ca25cf4af2691a6d0c58b7478618a
tamilselvanid <tamilselvanid@outlook.com>
2018-10-24 09:41:48 +0530

    Screen design for Spares Accessories done

---
+++ src/BikeShowRoom.WPF/Products/SparesAccessories.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ViewModel;
+
+namespace BikeShowRoom.WPF.Products
+{
+    /// <summary>
+    /// Interaction logic for ProductInsertWindow.xaml
+    /// </summary>
+    public partial class ProductWindow : Window
+    {
+        ProductViewModel viewmodel;
+        public ProductWindow()
+        {
+            try { 
+            InitializeComponent();
+            viewmodel = new ProductViewModel();
+            viewmodel.InsertCommand.IsEnabled = true;
+            base.DataContext = viewmodel;
+            }
+            catch(Exception ex)
+            {
+                if(ex.InnerException==null)
+                { 
+                MessageBox.Show("Error 1" + ex.Message);
+                }
+                else
+                {
+                    MessageBox.Show("Error 2" + ex.InnerException.Message);
+
+                    if(ex.InnerException.InnerException!=null)
+                    {
+                        MessageBox.Show("Error 3" + ex.InnerException.InnerException.Message);
+                    }
+                }
+            }
+        }
+
+        
+
+      
+    }
+}
