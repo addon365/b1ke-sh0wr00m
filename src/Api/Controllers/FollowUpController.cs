dee00fa0a4bb05cec3d8d0a548bf851d813b15ef
Sathish Babu <sathishbabur@hotmail.com>
2018-11-15 23:35:54 +0530

    Removed authorize attribute to workaround issue
    Also removed unnecessary usings

--- src/Api/Controllers/FollowUpController.cs
+++ src/Api/Controllers/FollowUpController.cs
@@ -12,7 +12,7 @@ using swcApi.Utils;
 
 namespace swcApi.Controllers
 {
-    [Authorize]
+    
     [Produces("application/json")]
     [Route("api/{license:license}/v{version:apiVersion}/[controller]")]
     public class FollowUpController : Controller
