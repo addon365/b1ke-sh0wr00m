c3964117f9734fa150d6534fa9ab511a364b8bfc
tamilselvanid <tamilselvanid@outlook.com>
2019-09-16 18:07:58 +0530

    catalog license

--- src/addon365.Database.Entity/Crm/Customer.cs
+++ src/addon365.Database.Entity/Crm/Customer.cs
@@ -6,6 +6,7 @@ namespace addon365.Database.Entity.Crm
     {
         public string Identifier { get; set; }
         public Contact Profile { get; set; }
+        public BusinessContact BusinessContact { get; set; }
         public Guid? UserId { get; set; }
     }
 }
---
+++ src/addon365.Database.Entity/Inventory/Catalog/CatalogLicenseMaster.cs
@@ -0,0 +1,17 @@
+﻿using addon365.Database.Entity.Crm;
+using addon365.Database.Entity.Inventory.Sales;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace addon365.Database.Entity.Inventory.Catalog
+{
+    public class CatalogLicenseMaster:BaseEntity
+    {
+        string CatalogLicenseId { get; set; }
+        Customer Customer { get; set; }
+        CatalogItem MainItem { get; set; }
+        Sale Sale { get; set; }
+
+    }
+}
